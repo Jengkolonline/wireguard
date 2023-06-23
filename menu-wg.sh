9356e9ea8030f001b494e5e211ce26cf2e01d44f
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-06-24 05:45:58 +0700

    Update and rename menu-vless.sh to menu-wg.sh

--- menu-wg.sh
+++ menu-wg.sh
@@ -26,14 +26,23 @@ checking_sc() {
 checking_sc
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m       • XRAY / VLESS MENU •         \E[0m"
+echo -e "\E[0;100;33m       • Wireguard MENU •         \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Create Account XRAY Vless Websocket "
-echo -e " [\e[36m•2\e[0m] Trial Account XRAY Vless "
-echo -e " [\e[36m•3\e[0m] Extending Account XRAY Vless Active Life "
-echo -e " [\e[36m•4\e[0m] Delete Account XRAY Vless Websocket "
-echo -e " [\e[36m•5\e[0m] Check User Login XRAY Vless "
+echo -e " [\e[36m•1\e[0m] Create Account wireguard "
+#echo -e " [\e[36m•2\e[0m] Trial Account XRAY Vless "
+echo -e " [\e[36m•2\e[0m] Extending Account wireguard "
+echo -e " [\e[36m•3\e[0m] Delete Account wireguard "
+#echo -e " [\e[36m•5\e[0m] Check User Login XRAY Vless "
+echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m       • Wireguard Install •         \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•4\e[0m] Install wireguard "
+echo -e " [\e[36m•5\e[0m] Uninstall wireguard "
+echo -e ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
@@ -44,11 +53,11 @@ echo -e ""
 read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
-1) clear ; add-vless ; exit ;;
-2) clear ; trialvless ; exit ;;
-3) clear ; renew-vless ; exit ;;
-4) clear ; del-vless ; exit ;;
-5) clear ; cek-vless ; exit ;;
+1) clear ; addwg ; exit ;;
+2) clear ; renewwg ; exit ;;
+3) clear ; delwg ; exit ;;
+4) clear ; wg ; exit ;;
+5) clear ; res ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu-ssh ;;
